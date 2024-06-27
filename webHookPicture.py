8b07bfecda8861d882cbe75a7db62471b2108f2a
= <=>
2024-06-27 11:52:48 +0200

    using stdin stder stdout

--- webHookPicture.py
+++ webHookPicture.py
@@ -1,6 +1,7 @@
-import requests
 import os
+import sys
 import logging
+import requests
 from dotenv import load_dotenv
 
 logging.basicConfig(
@@ -13,7 +14,7 @@ logging.basicConfig(
 
 
 def configure():
-    """starts load_dotenv() function"""
+    """Starts load_dotenv() function"""
     load_dotenv()
 
 
@@ -22,17 +23,28 @@ def WebHook(url: str):
     Sends picture through discord webhook.
     url (str) - url of your discord webhook
     """
+    sys.stderr = open("app.log", "a")
+    sys.stdout = open("app.log", "a")
 
     for i in range(1, 4):
         try:
             with open(f"slika{i}.jpg", "rb") as f:
-                # open("your picture", "rb") as f:...
                 file = {"file": f}
                 response = requests.post(url, files=file)
                 if response.status_code != 200:
-                    print(response.status_code)
+                    sys.stderr.write(
+                        f"Failed to send file slika{i}.jpg. HTTP status code: {response.status_code}\n"
+                    )
+                else:
+                    sys.stdout.write(f"Successfully sent file slika{i}.jpg.\n")
         except FileNotFoundError:
-            logging.error("FileNotFound Error happened!", exc_info=True)
+            sys.stderr.write(f"File slika{i}.jpg not found\n")
+            sys.stdout.write(f"File slika{i}.jpg not found\n")
+        except Exception as e:
+            logging.error("An error occurred", exc_info=True)
+
+    sys.stderr.close()
+    sys.stdout.close()
 
 
 def main():
@@ -41,13 +53,12 @@ def main():
     Raises:
         ValueError: Raises valueError exception when token_id or channel id is None.
     """
+    configure()
 
     channel_id = os.getenv("webhook_id")
     token_id = os.getenv("webhook_token")
     if token_id is None or channel_id is None:
         raise ValueError("Channel_id or token_id is None! Check your .env file again.")
-
-    configure()
 
     url = f"https://discord.com/api/webhooks/{channel_id}/{token_id}"
     WebHook(url)
