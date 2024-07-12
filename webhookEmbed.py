8908c2ac1bd4af1515c837b96502aae71b7657af
Vuk <129513318+shakal19@users.noreply.github.com>
2024-07-12 10:37:30 +0200

    Add files via upload
    Version 2.0

--- webhookEmbed.py
+++ webhookEmbed.py
@@ -3,6 +3,47 @@ import json
 import requests
 from dotenv import load_dotenv
 
+# from file_reader import file_reader_and_writer
+
+
+def parsing_json(path: str):
+    """
+    Parsing json files
+
+    Args:
+        path (str): filename
+    """
+    try:
+
+        with open(path, "r") as json_file:
+            data = json.load(json_file)
+            return data
+    except FileNotFoundError as e:
+        print("Exception message:", e)
+
+
+def convert_to_json(files):
+    """
+    Converts python object to json format
+
+    Args:
+        files (list): a list with filenames
+
+    Returns:
+        python object: returns parsed combined json file
+    """
+    embeds = []
+    for i in range(len(files)):
+        embed = parsing_json(files[i])
+        embeds.append(embed)
+
+    combined_json = {"embeds": embeds}
+    with open("combined.json", "w") as f_out:
+        json.dump(combined_json, f_out, indent=4)
+
+    data = parsing_json("combined.json")
+    return data
+
 
 def webhook_embed(url: str):
     """
@@ -12,12 +53,15 @@ def webhook_embed(url: str):
         url (str): webhook url
     """
 
-    with open("embed.json", "r") as f:
-        data = json.load(f)
+    files = ["embed2.json", "embed3.json"]
 
-        response = requests.post(url, json=data)
-        if response.status_code == 204:
-            print("Message sent successfully")
+    data = convert_to_json(files)
+
+    response = requests.post(url, json=data)
+    if response.status_code == 204:
+        print("Message sent successfully")
+    else:
+        print(response.status_code)
 
 
 def main() -> None:
